c4e320f95f33865fb2ff431e354b29a56084d0c1
Kausik Das <kausik007007@gmail.com>
2020-09-19 11:07:52 +0530

    new subs form designed and attached with mailchimp

---
+++ themes/anatole/assets/js/popup-form.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("popup").style.display = "block";
+  }
+  
+function closeForm() {
+    document.getElementById("popup").style.display = "none";
+  }
